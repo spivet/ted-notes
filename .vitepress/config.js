a650942de900c6a95628fa9d2189061effef2b52
Spivet <371135482@qq.com>
2022-10-27 13:09:44 +0800

    chore: 使用GitHub action自动构建

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,7 +1,8 @@
 export default {
     lang: 'en-US',
+    base: '/ted-notes/',
     srcDir: './src',
-    outDir: './public',
+    outDir: './dist',
     title: 'TED Notes',
     description: 'Lessons worth sharing!',
 
