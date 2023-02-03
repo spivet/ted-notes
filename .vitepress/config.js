4a2d75d6b4bb13348808eae2a939bd25f8926904
Spivet <371135482@qq.com>
2023-02-03 16:09:47 +0800

    feat: 添加百度统计

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -5,6 +5,20 @@ export default {
     outDir: './dist',
     title: 'TED Notes',
     description: 'Lessons worth sharing!',
+    head: [
+      [
+        'script',
+        {},
+        `var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?d53b187ff570da628cb1150db22bc250";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+        `
+      ]
+    ],
 
     themeConfig: {
         nav: [
